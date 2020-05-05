1bd6e2b63e8aa0b191bc8f6028d46569ff2dde6b
skyways <amoskrr@gmail.com>
2020-05-05 08:21:10 +0300

    initial commit

---
+++ app/src/main/java/com/fastlane_circle_ci/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fastlane_circle_ci
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
